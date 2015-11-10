479710f7d7340832adebfdc2e9b08c2181ea69a8
Florent Gouget <florent.gouget@gmail.com>
2015-11-10 14:03:59 +0100

    getNew Method

--- src/GenericDao.js
+++ src/GenericDao.js
@@ -9,6 +9,10 @@ export default function GenericDao(model){
 
         }
 
+        getNew(){
+            return new self.model();
+        }
+
         setQuery(query){
             this.opts            = this.opts || {};
             this.opts.conditions = this.opts.conditions || {};
