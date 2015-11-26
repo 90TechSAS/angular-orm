56d01610a7a38269b3b92f3e497bacc4b4793d88
Florent Gouget <florent.gouget@gmail.com>
2015-11-26 17:37:08 +0100

    93% coverage \o/

--- tst/specs/angular-dao.specs.js
+++ tst/specs/angular-dao.specs.js
@@ -12,7 +12,7 @@ describe('Angular DAO', function(){
         inject(function(_ModelManager_, $httpBackend, _$rootScope_){
             ModelManager = _ModelManager_;
             httpBackend  = $httpBackend;
-            $rootScope = _$rootScope_;
+            $rootScope   = _$rootScope_;
 
 
         })
@@ -48,6 +48,23 @@ describe('Angular DAO', function(){
         expect(qb).toBeDefined();
     });
 
+    it('Should have created field specific methods', function(){
+        httpBackend.expectGET(encodeURI('http://MOCKURL.com/model1?conditions={"label":"toto"}')).respond([]);
+        ModelManager.selectByLabel('toto');
+        httpBackend.flush();
+    });
+
+    it('Should have created field specific methods and extract object', function(){
+        httpBackend.expectGET(encodeURI('http://MOCKURL.com/model1?conditions={"model2":"2"}')).respond([]);
+        ModelManager.selectByModel2([{_id: "2"}]);
+        httpBackend.flush();
+    });
+    it('Should have created field specific methods and extract object with key', function(){
+        httpBackend.expectGET(encodeURI('http://MOCKURL.com/model1?conditions={"model2":"2"}')).respond([]);
+        ModelManager.selectByModel2(["2"]);
+        httpBackend.flush();
+    });
+
     it('Should return an ActiveRecord element', function(){
         httpBackend.expectGET('http://MOCKURL.com/model1/123').respond({
             _id: '1234656'
@@ -59,6 +76,64 @@ describe('Angular DAO', function(){
 
     });
 
+    it('Should query by Id if not key specified', function(){
+        httpBackend.expectGET(encodeURI('http://MOCKURL.com/model1?conditions={"_id":"2"}')).respond([]);
+        ModelManager.get(ModelManager.query().select('2'));
+        httpBackend.flush();
+    });
+
+    it('Should query directly if array size === 1', function(){
+        httpBackend.expectGET(encodeURI('http://MOCKURL.com/model1?conditions={"_id":"2"}')).respond([]);
+        ModelManager.get(ModelManager.query().select(['2']));
+        httpBackend.flush();
+    });
+    it('Should query with $in operator', function(){
+        httpBackend.expectGET(encodeURI('http://MOCKURL.com/model1?conditions={"_id":{"$in":["2","3"]}}')).respond([]);
+        ModelManager.get(ModelManager.query().select(['2', '3']));
+        httpBackend.flush();
+    });
+
+    it('Should query on another field if specified', function(){
+        httpBackend.expectGET(encodeURI('http://MOCKURL.com/model1?conditions={"label":"2"}')).respond([]);
+        ModelManager.get(ModelManager.query().select(['2'], 'label'));
+        httpBackend.flush();
+    });
+
+    it ('Should query for archives', function(){
+        httpBackend.expectGET(encodeURI('http://MOCKURL.com/model1?archived=true')).respond([]);
+        ModelManager.get(ModelManager.query().archived(true));
+        httpBackend.flush();
+
+    });
+    it ('Should paginate', function(){
+        httpBackend.expectGET(encodeURI('http://MOCKURL.com/model1?limit=10&skip=5')).respond([]);
+        ModelManager.get(ModelManager.query().paginate({skip:5, limit:10}));
+        httpBackend.flush();
+    });
+    it ('Should limit', function(){
+        httpBackend.expectGET(encodeURI('http://MOCKURL.com/model1?limit=10')).respond([]);
+        ModelManager.get(ModelManager.query().limit(10));
+        httpBackend.flush();
+    });
+
+    it ('Should sort', function(){
+        httpBackend.expectGET(encodeURI('http://MOCKURL.com/model1?sort=toto')).respond([]);
+        ModelManager.get(ModelManager.query().sort("toto"));
+        httpBackend.flush();
+    });
+
+    it ('Should select specific fields', function(){
+        httpBackend.expectGET(encodeURI('http://MOCKURL.com/model1?select=toto+tutu')).respond([]);
+        ModelManager.get(ModelManager.query().fields(["toto", "tutu"]));
+        httpBackend.flush();
+    });
+
+    it ('Should search', function(){
+        httpBackend.expectGET(encodeURI('http://MOCKURL.com/model1?conditions={"name":{"$regex":".*toto.*","$options":"i"}}')).respond([]);
+        ModelManager.get(ModelManager.query().search('toto'));
+        httpBackend.flush();
+    });
+
     it('Should make subPopulate queries', function(){
         var model = ModelManager.create({
             _id   : '1234656',
@@ -67,9 +142,27 @@ describe('Angular DAO', function(){
         httpBackend.expectGET('http://MOCKURL.com/model2/77777').respond();
         model.populate('model2');
         httpBackend.flush();
-       // $rootScope.$digest();
+        // $rootScope.$digest();
 
     });
+
+    it('Should save objects', function(){
+        var model = ModelManager.createModel({
+            _id   : '1234656',
+            model2: '77777'
+        });
+        httpBackend.expectPUT('http://MOCKURL.com/model1/1234656').respond();
+        model.save();
+        httpBackend.flush();
+    });
+
+
+    it('Should make a query with populate', function(){
+        httpBackend.expectGET(encodeURI('http://MOCKURL.com/model1?populate=[{"path":"model2"}]')).respond([]);
+        ModelManager.get(ModelManager.query().populate('model2'));
+        httpBackend.flush();
+
+    })
 
 
 });
